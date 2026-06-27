7d364c0cf8df726c01f90351760c9329a22e37b2
bogdanm <12345678+CostaLacoste@users.noreply.github.com>
2026-06-27 17:20:23 +0400

    Seventh

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from '../../Components/Layouts/Layout.js'
 import '../../Styles/HomeStyle.css'
 import Section1 from './Section1.js'
+import Section2 from './Section2.js';
 
 function Home() {
     return (
         <>
         <Layout>
             <Section1 />
+            <Section2 />
         </Layout>
         </>
     );
