fc614b5c4137e2d7651cdb2b8064c410b7eab436
bogdanm <12345678+CostaLacoste@users.noreply.github.com>
2026-06-27 17:20:23 +0400

    Promotion section is done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,7 @@ import '../../Styles/HomeStyle.css'
 import Section1 from './Section1.js'
 import Section2 from './Section2.js';
 import Section3 from './Section3.js';
+import Section4 from './Section4.js';
 
 function Home() {
     return (
@@ -12,6 +13,7 @@ function Home() {
             <Section1 />
             <Section2 />
             <Section3 />
+            <Section4 />
         </Layout>
         </>
     );
