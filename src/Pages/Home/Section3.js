68609fa3a7f48585dd3cec5976e34c0befe6283b
bogdanm <12345678+CostaLacoste@users.noreply.github.com>
2026-06-27 17:20:23 +0400

    Menu fix 2

--- src/Pages/Home/Section3.js
+++ src/Pages/Home/Section3.js
@@ -84,13 +84,13 @@ const renderRatingIcons = (rating) => {
 
     for(let i = 0; i < 5; i++){
         if(rating > 0.5){
-            stars.push(<i key={i} className='bi bi-stars-fill'></i>);
+            stars.push(<i key={i} className='bi bi-star-fill'></i>);
             rating--;
         } else if (rating > 0 && rating < 1){
-            stars.push(<i key={'half'} className='bi bi-stars-half'></i>);
+            stars.push(<i key={'half'} className='bi bi-star-half'></i>);
             rating--;
         } else {
-            stars.push(<i key={`empty${i}`} className='bi bi-stars'></i>);
+            stars.push(<i key={`empty${i}`} className='bi bi-star'></i>);
         }
     }
     return stars;
